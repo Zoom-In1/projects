164dcdfa0c12e56a6cfe1a53e299bc9bb044161b
Zoom-In1 <zoomin0595@naver.com>
2024-12-10 23:25:32 +0900

    이메일 인증 SMTP

---
+++ Coffeeshop(spring_boot)/CoffeeShop/src/main/java/com/example/CoffeeShop/controller/MailController.java
@@ -0,0 +1,9 @@
+package com.example.CoffeeShop.controller;
+
+public class MailController {
+	
+	// 메일 보내기
+	
+	// 메일 번호 인증하기
+
+}
---
+++ Coffeeshop(spring_boot)/CoffeeShop/src/main/java/com/example/CoffeeShop/jwt/KakaoAPI.java
@@ -0,0 +1,11 @@
+package com.example.CoffeeShop.jwt;
+
+public class KakaoAPI {
+	
+	// 카카오톡 로그인 
+	
+	// 카카오톡 맵
+	
+	// 카카오 모바일결제
+
+}
